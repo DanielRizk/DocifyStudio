9d357b3e53fbfda0fd9b7de86b1bfa279cfe4b9e
DanielRizk <daniel.rizk96@gmail.com>
2023-11-28 15:15:59 +0100

    Update UI

---
+++ src/com/daniel/docify/core/Main.java
@@ -0,0 +1,12 @@
+package com.daniel.docify.core;
+
+import com.daniel.docify.ui.MainWindow;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        MainWindow mainWindow = new MainWindow();
+    }
+}
